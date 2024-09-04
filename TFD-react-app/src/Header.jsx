b3d2a56d88f279e9c2b03d58192dce5f7c3d52ba
Irwin Felicien <felicienirwin@outlook.com>
2024-09-03 19:58:17 -0500

    made changes to react structure

---
+++ TFD-react-app/src/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css';
+
+export default function Header(){
+    return(
+        <>
+        <header>
+          <h1>The First Descendent</h1>
+          <h2>Lookup and Character Builder</h2>
+          <h3>Coming Soon</h3>
+        </header>
+        <hr />
+      </>
+    );
+}
